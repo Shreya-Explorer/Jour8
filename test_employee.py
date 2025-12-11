611727cfdd486bf1d9c42528d905892a19a90cc0
Shreya Raykar <shreyaraikar2304@gmail.com>
2025-12-11 15:26:05 +0530

    Fix formatting in test_employee.py

--- test_employee.py
+++ test_employee.py
@@ -4,7 +4,7 @@ def test_employee_details():
     expected_output = (
         "Employee Name : Shreya\n"
         "Employee ID : 01FE24BCA150\n"
-        "Department : BCA\n "
+        "Department : BCA\n"
         "Salary : 80000"
     )
     assert employee_details("Shreya","01FE24BCA150","BCA",80000) == expected_output
