e330c1b9e1d74d31dfa4da40e0f5fbb85d14be84
Shreya Raykar <shreyaraikar2304@gmail.com>
2025-12-11 15:40:18 +0530

    Fix formatting of employee name output

--- employee.py
+++ employee.py
@@ -1,6 +1,6 @@
 def employee_details(name,emp_id,department,salary):
     result = (
-        f"Employee_Name:{name} \n"
+        f"Employee_Name:{name}\n"
         f"Employee_ID:{emp_id}\n"
         f"Department:{department}\n"
         f"Salary:{salary}"
