61f48e1049d5cd14fef5ea28a8db04350261452e
Shreya Raykar <shreyaraikar2304@gmail.com>
2026-01-01 21:05:50 +0530

    Update test for employee details with new data

--- test_employee.py
+++ test_employee.py
@@ -1,9 +1,9 @@
 from employee import employee_details
 def test_employee_details():
     expected_output = (
-        "Employee_Name:Nitya Savadatti\n"
-        "Employee_ID:01FE24BCA174\n"
+        "Employee_Name:Sinchana Naik\n"
+        "Employee_ID:01FE24BCA169\n"
         "Department:BCA\n"
         "Salary:80000"
     )
-    assert employee_details("Nitya Savadatti","01FE24BCA174","BCA",80000)==expected_output
+    assert employee_details("Sinchana Naik","01FE24BCA169","BCA",80000)==expected_output
